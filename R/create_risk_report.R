b3be45c1d47a61b98812d8435926f435685f0d8f
owenssam1_SERC <sam.owens@temple.edu>
2024-05-07 18:32:36 -0400

    began create_risk_report.R
    function will create report of major findings by locality (state or country)

--- R/create_risk_report.R
+++ R/create_risk_report.R
@@ -7,7 +7,10 @@
 #'
 #'@details
 #'
-#'Requires the following packages: 'tidyverse', 'cli'.
+#'Requires the following packages: 'tidyverse', 'cli', rnaturalearthhires.
+#'
+#'Note that this function performs downloads from [naturalearthdata.com](https://www.naturalearthdata.com/).
+#'The function will automatically create subfolders in "root/data-raw" containing the shapefiles.
 #'
 #'@return
 #'
@@ -28,7 +31,39 @@ create_risk_report <- function(locality) {
 
 
 
-  # import shapefiles
+  # import shapefiles (locality)------------------------------------------------
+
+  # update packages
+  rnaturalearth::check_rnaturalearthdata()
+  rnaturalearth::check_rnaturalearthhires()
+
+  # create directories for shapefiles
+  dir.create(here::here(), "data-raw", "ne_countries")
+  dir.create(here::here(), "data-raw", "ne_states_provinces")
+
+
+  # import countries
+  countries_sf <- rnaturalearth::ne_download(
+    scale = 10, # highest resolution
+    type = "admin_0_countries", # states and provinces
+    category = "cultural",
+    destdir = file.path(here::here(), "data-raw", "ne_countries"),
+    load = TRUE, # load into environment
+    returnclass = "sf" # shapefile
+  )
+
+  # import states
+  states_provinces_sf <- rnaturalearth::ne_download(
+    scale = 10, # highest resolution
+    type = "admin_1_states_provinces", # states and provinces
+    category = "cultural",
+    destdir = file.path(here::here(), "data-raw", "ne_states_provinces"),
+    load = TRUE, # load into environment
+    returnclass = "sf" # shapefile
+  )
+
+
+
 
 
 
